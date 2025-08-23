16490260a0636f1ac41f2d1a283c7258f1cdaa25
Salim Ansari <salim.ansari@nw18.com>
2025-08-23 22:16:32 +0530

    eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off", // disable globally
+      "@next/next/no-html-link-for-pages": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
---
+++ src/app/category/[categorySlug]/[slug]/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ArticleConsumption() {
+    return (
+        <>
+        <p>TODO: Article detail page</p>
+        </>
+    );
+}
